6f8f3821a5e85cdbf5e63f5d2794c0dcf8f75b51
ryuqae <ryuqae@gmail.com>
2021-07-29 17:42:03 +0900

    add : fake headers

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import pandas as pd
 import argparse
 from datetime import datetime
 import time, random
+import multiprocessing
+from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+from fake_headers import Headers
 
 
 parser = argparse.ArgumentParser(description="Process some integers.")
@@ -22,7 +25,37 @@ headers = {
 class WebCrawler:
     def __init__(self) -> None:
         self.base_url = f"https://www.ddanzi.com/index.php?mid=free&"
-        pass
+        self.proxy = self.proxy_create()
+
+    def proxy_create(self):
+        """
+        무작위로 프록시를 생성해서 가져오는 코드
+        """
+        self.req_proxy = RequestProxy()
+        proxy = self.test_proxy()  # 잘 작동되는 프록시 선별
+        return proxy
+
+    def test_proxy(self):
+        """
+        가져온 프록시중에서 실제로 작동되는 프록시만 하나씩 가져오는 코드
+        test_url : 자신의 IP를 확인하는 코드. 여기서 변경된 IP가 나오면 성공적으로 우회가된것
+        """
+
+        test_url = "http://ipv4.icanhazip.com"
+        while True:  # 제대로된 프록시가 나올때까지 무한반복
+            requests = self.req_proxy.generate_proxied_request(test_url)
+
+            if requests is not None:
+                print(
+                    "\t Response: ip={0}".format("".join(requests.text).encode("utf-8"))
+                )
+                proxy = self.req_proxy.current_proxy
+                break
+
+            else:
+                continue
+
+        return proxy  # 잘작동된 proxy를 뽑아준다.
 
     # @profile
     def get_document_srl_per_page(self, params: dict) -> list:
@@ -38,14 +71,14 @@ class WebCrawler:
 
         return pd.DataFrame(
             [
-                self._get_contents(parse.parse_qs(link["href"])["document_srl"][0])
+                self.get_contents(parse.parse_qs(link["href"])["document_srl"][0])
                 for link in links[4:]
             ],
             columns=["id", "title", "text", "time"],
         )
 
     # @profile
-    def _get_contents(self, document_srl: int) -> dict:
+    def get_contents(self, document_srl: int) -> dict:
         post_url = f"https://www.ddanzi.com/free/{document_srl}"
         result = requests.get(post_url, headers=headers)
         result_html = result.text
@@ -65,8 +98,22 @@ class WebCrawler:
 
         return document_srl, title, text, time
 
+    def crawling(self):
+        header = Headers(
+            browser="chrome",  # Generate only Chrome UA
+            os="win",  # Generate ony Windows platform
+            headers=True,  # generate misc headers
+        )
+        self.headers = header.generate()  # 랜덤 유저 에이전트를 생성해주는 함수.
+        _url = "https://www.ddanzi.com/free"
 
-# 189416
+        self.proxies = {}  # request.get 인자에 넣어줄 딕셔너리 생성
+        self.proxies["http"] = "http://%s" % self.proxy
+
+        html = requests.get(_url, headers=self.headers, proxies=self.proxies).content
+        print(html)
+
+
 if __name__ == "__main__":
     start_timestamp = datetime.now()
     print(f"Crawling DDANZI from {start} to {end}")
@@ -87,4 +134,4 @@ if __name__ == "__main__":
         print(
             f"{ith} pages scraped: {round(ith/(end-start)*100, 4)}%, {now_timestamp-start_timestamp} passed."
         )
-        time.sleep(random.uniform(10, 30))
+        time.sleep(random.uniform(0, 5))
